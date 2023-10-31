978956c283c851f61c8fea07bb7c7f3b49b9e514
CristopherAreche <cristopherareche764@gmail.com>
2023-10-31 15:59:38 -0400

    image loading test 2

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,7 +18,7 @@ const Skills = () => {
             >
               <div className="grid grid-cols-2 gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={image} width={50} height={50} alt="/" />
+                  <Image src={image} width={50} height={50} alt={name} />
                 </div>
                 <div>
                   <h3 className="flex flex-col items-center justify-center">
